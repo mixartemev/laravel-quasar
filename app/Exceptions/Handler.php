b195a58a8e2f18379b5cb2c3d1f242752987db1d
mix <mixartemev@gmail.com>
2020-05-31 22:50:55 +0300

    errors in json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        return response()->json($exception->getMessage(), $exception->status);
     }
 }
